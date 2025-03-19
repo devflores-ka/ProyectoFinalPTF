0f0e7df8a2a028c3f43765aa45fc867402e6dc4a
Darun <a.catapaz@gmail.com>
2025-03-19 17:00:49 -0300

    orden controladores

--- src/main/java/com/equipo6/controladores/ControladorClientes.java
+++ src/main/java/com/equipo6/controladores/ControladorClientes.java
@@ -1,4 +1,5 @@
 package com.equipo6.controladores;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,174 +26,165 @@ import jakarta.validation.Valid;
 @RequestMapping("/cliente")
 @Controller
 public class ControladorClientes {
-	
+
 	@Autowired
 	private ServicioUsuarios sUsuarios;
-	
+
 	@Autowired
 	private ServicioPedido sPedido;
-	
+
 	@Autowired
 	private ServicioProductos sProductos;
-	
+
 //AClistaProductos.jsp
-    @GetMapping("/home")
-    public String home (HttpSession session, Model model) {
-    	
-    	if (session.getAttribute("usuarioEnSesion") == null ) {
-		    return "redirect:/";
+	@GetMapping("/home")
+	public String home(HttpSession session, Model model) {
+
+		if (session.getAttribute("usuarioEnSesion") == null) {
+			return "redirect:/";
 		}
-    	
-    	String admin = "ADMIN";
+
+		String admin = "ADMIN";
 		List<Producto> productos = sProductos.todosLosProductos();
 		model.addAttribute("admin", admin);
 		model.addAttribute("productos", productos);
-		
-    	return "AClistaProductos.jsp"; //admin/productos 
-    	
-    }
-	
+
+		return "AClistaProductos.jsp"; // admin/productos
+
+	}
+
 //ACEdetalleProducto.jsp
 	@GetMapping("/productos/{id}")
-	public String mostrar(@PathVariable("id") Long id,
-						  Model model,
-						  HttpSession session) {
-		
-		if(session.getAttribute("usuarioEnSesion") == null){
+	public String mostrar(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		if (session.getAttribute("usuarioEnSesion") == null) {
 			return "redirect:/";
 		}
-		
-		String cliente = "CLIENTE";//para comparacion en jsp
+
+		String cliente = "CLIENTE";// para comparacion en jsp
 		model.addAttribute("cliente", cliente);
 		String admin = "ADMIN";
 		model.addAttribute("admin", admin);
-		
+
 		Producto producto = sProductos.buscarProducto(id);
 		if (producto == null) {
 			return "redirect:/cliente/home";
 		}
-		
-		model.addAttribute("producto", producto); 
-		
+
+		model.addAttribute("producto", producto);
+
 		return "ACEdetalleProducto.jsp";
-		
+
 	}
+
 //ACEpedidos.jsp
 	@GetMapping("/pedidos")
 	public String pedidos(HttpSession session, Model model) {
-		
-		if (session.getAttribute("usuarioEnSesion") == null ) {
-		    return "redirect:/";
+
+		if (session.getAttribute("usuarioEnSesion") == null) {
+			return "redirect:/";
 		}
-		
-		String cliente = "CLIENTE";//para comparacion en jsp
+
+		String cliente = "CLIENTE";// para comparacion en jsp
 		model.addAttribute("cliente", cliente);
 		String admin = "ADMIN";
 		model.addAttribute("admin", admin);
-		
-		//Para conseguir los pedidos del USUARIO EN SESION
-		Usuario usuarioEnSesion = (Usuario)session.getAttribute("usuarioEnSesion"); //Obteniendo de la sesión el objeto usuario
+
+		// Para conseguir los pedidos del USUARIO EN SESION
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion"); // Obteniendo de la sesión el
+																						// objeto usuario
 		Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
 		model.addAttribute("usuario", usuario);
-		
-		
-		return "ACElistaPedidos.jsp"; 
+
+		return "ACElistaPedidos.jsp";
 	}
+
 //ACEdetallePedido.jsp	
 	@GetMapping("/pedidos/{id}")
-	public String mostrarPedido (@PathVariable("id") Long id,
-						  Model model,
-						  HttpSession session) {
-		
-		if(session.getAttribute("usuarioEnSesion") == null){
+	public String mostrarPedido(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		if (session.getAttribute("usuarioEnSesion") == null) {
 			return "redirect:/";
 		}
-		
-		Pedido pedido = sPedido.buscarPedido(id); 
-		model.addAttribute("pedido", pedido); 
-		
+
+		Pedido pedido = sPedido.buscarPedido(id);
+		model.addAttribute("pedido", pedido);
+
 		Usuario usuarioEnSesion = (Usuario) session.getAttribute("userInSession");
 		Usuario usuarioActualizado = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
-		
+
 		model.addAttribute("usuario", usuarioActualizado);
-		
-		return "ACEdetallePedido.jsp"; //jsp vista 5 detalle de un  pedido
-		
+
+		return "ACEdetallePedido.jsp"; // jsp vista 5 detalle de un pedido
+
 	}
-	
+
+	// DELETEmappind
+	@DeleteMapping("/borrar/pedido/{id}")
+	public String borrarPedido(@PathVariable("id") Long id) {
+		sPedido.borrarPedido(id);
+
+		return "redirect:/cliente/pedidos";
+	}
+
 //ACdetalleCliente.jsp
 	@GetMapping("/{id}")
-	public String mostrarCliente (@PathVariable("id") Long id,
-						  Model model,
-						  HttpSession session) {
-		
-		if(session.getAttribute("usuarioEnSesion") == null){
+	public String mostrarCliente(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		if (session.getAttribute("usuarioEnSesion") == null) {
 			return "redirect:/";
 		}
-		
+
 		String admin = "ADMIN";
 		model.addAttribute("admin", admin);
-		
-		Usuario usuarioEnSesion = (Usuario)session.getAttribute("usuarioEnSesion");
-		
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
 		Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
-		if(usuario == null) {
+		if (usuario == null) {
 			return "redirect:/cliente/home";
 		}
 		model.addAttribute("usuario", usuario);
-		
-		return "ACdetalleCliente.jsp"; //admin/clientes/{id}
-		
+
+		return "ACdetalleCliente.jsp"; // admin/clientes/{id}
+
 	}
-//DELETEmappind
-	@DeleteMapping("/borrar/pedido/{id}")
-	public String borrarPedido(@PathVariable("id")Long id) {
-		sPedido.borrarPedido(id);
-		
-		return "redirect:/cliente/pedidos";
-	}
+
 //ACeditarCliente.jsp
 	@GetMapping("/editar/{id}")
-	public String editar(@ModelAttribute("cliente") Usuario cliente,
-			                              @PathVariable("id") Long id,
-                                           Model model,
-                                           HttpSession session) {
-		if(session.getAttribute("usuarioEnSesion") == null){
+	public String editar(@ModelAttribute("cliente") Usuario cliente, @PathVariable("id") Long id, Model model,
+			HttpSession session) {
+		if (session.getAttribute("usuarioEnSesion") == null) {
 			return "redirect:/";
 		}
 		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
 		Usuario usuarioActual = sUsuarios.buscarUsuario(id);
-		
-		if(usuarioActual == null || !usuarioActual.getId().equals(usuarioEnSesion.getId())) {
-			return "redirect:/cliente/"+usuarioEnSesion.getId();
+
+		if (usuarioActual == null || !usuarioActual.getId().equals(usuarioEnSesion.getId())) {
+			return "redirect:/cliente/" + usuarioEnSesion.getId();
 		}
-		
-		String admin = "ADMIN";//para comparacion en jsp
+
+		String admin = "ADMIN";// para comparacion en jsp
 		model.addAttribute("admin", admin);
-		
-		
+
 		model.addAttribute("usuario ", usuarioActual);
-	
-		return"ACeditarCliente.jsp"; 
-	
-   }
+
+		return "ACeditarCliente.jsp";
+
+	}
 
 	@PutMapping("/actualizar/{id}")
-	public String procesarEditarReceta(@Valid @ModelAttribute("usuario")Usuario usuario,
-			                            @PathVariable("id")Long id,
-			                            BindingResult result
-			                            ) {
-		if(result.hasErrors()) {
-			System.out.println("hubo un error"+result);
+	public String procesarEditarReceta(@Valid @ModelAttribute("usuario") Usuario usuario, @PathVariable("id") Long id,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			System.out.println("hubo un error" + result);
 			return "CLIENTEeditarCliente.jsp";
-		}else { 
-	      
-			
+		} else {
+
 			sUsuarios.guardarUsuario(usuario);
-			return "redirect:/mostrarcliente/{id}"; //vista 7 de edicion
-		
-	 }
-	
+			return "redirect:/mostrarcliente/{id}"; // vista 7 de edicion
+
+		}
+
 	}
 }
-	
--- src/main/java/com/equipo6/controladores/ControladorEmpresas.java
+++ src/main/java/com/equipo6/controladores/ControladorEmpresas.java
@@ -27,236 +27,250 @@ import jakarta.validation.Valid;
 @RequestMapping("/empresa")
 @Controller
 public class ControladorEmpresas {
-    
-    @Autowired
-    private ServicioUsuarios sUsuarios;
-    
-    @Autowired
-    private ServicioProductos sProductos;
-    
-    @Autowired
-    private ServicioPedido sPedidos;
+
+	@Autowired
+	private ServicioUsuarios sUsuarios;
+
+	@Autowired
+	private ServicioProductos sProductos;
+
+	@Autowired
+	private ServicioPedido sPedidos;
+
 //EMPRESAhome.jsp
-    @GetMapping("/home")
-    public String home(HttpSession session, Model model) {
-    	
-    	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-    	
-        if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-            return "redirect:/";
-        }
-        
-        return "EMPRESAhome.jsp";
-    }
+	@GetMapping("/home")
+	public String home(HttpSession session, Model model) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		return "EMPRESAhome.jsp";
+	}
+
 //ElistaProductos.jsp
-    @GetMapping("/productos")
-    public String listaProductos(HttpSession session, Model model) {
-    	
-    	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-    	
-        if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-            return "redirect:/";
-        }
-        //Para conseguir los productos de LA EMPRESA EN SESION
-        Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
-        model.addAttribute("usuario", usuario);
-        
-        List<Producto> productos = sProductos.todosLosProductos();
-        model.addAttribute("productos", productos);
-        return "ElistaProductos.jsp";
-    }
-    
+	@GetMapping("/productos")
+	public String listaProductos(HttpSession session, Model model) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+		// Para conseguir los productos de LA EMPRESA EN SESION
+		Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
+		model.addAttribute("usuario", usuario);
+
+		List<Producto> productos = sProductos.todosLosProductos();
+		model.addAttribute("productos", productos);
+		return "ElistaProductos.jsp";
+	}
+
 //ACEdetalleProducto.jsp
-    @GetMapping("/productos/{id}")
-    public String detalleProducto(@PathVariable("id") Long id, Model model, HttpSession session) {
-       
-    	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-    	
-        if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-            return "redirect:/";
-        }
-        
-        String cliente = "CLIENTE";//para comparacion en jsp
+	@GetMapping("/productos/{id}")
+	public String detalleProducto(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		String cliente = "CLIENTE";// para comparacion en jsp
 		model.addAttribute("cliente", cliente);
 		String admin = "ADMIN";
 		model.addAttribute("admin", admin);
-        
-        Producto producto = sProductos.buscarProducto(id);
-        if (producto == null) {
-            return "redirect:/empresa/productos";
-        }
-        
-        model.addAttribute("producto", producto);
-        
-        return "ACEdetalleProducto.jsp";
-    }
+
+		Producto producto = sProductos.buscarProducto(id);
+		if (producto == null) {
+			return "redirect:/empresa/productos";
+		}
+
+		model.addAttribute("producto", producto);
+
+		return "ACEdetalleProducto.jsp";
+	}
+
 //nuevoProducto.jsp    
-    @GetMapping("/nuevo/producto")
-    public String nuevoProducto(Model model, HttpSession session) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
-        model.addAttribute("producto", new Producto());
-        
-        return "nuevoProducto.jsp";
-    }
+	@GetMapping("/nuevo/producto")
+	public String nuevoProducto(Model model, HttpSession session) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		model.addAttribute("producto", new Producto());
+
+		return "nuevoProducto.jsp";
+	}
+
 //POSTmapping    
-    @PostMapping("/agregar/producto")
-    public String agregarProducto(@Valid @ModelAttribute("producto") Producto producto, BindingResult result) {
-    	
-        if (result.hasErrors()) {
-            return "nuevoProducto.jsp";
-        } else {
-        	sProductos.guardarProducto(producto);
-	        return "redirect:/empresa/productos";
-        }
-    }
+	@PostMapping("/agregar/producto")
+	public String agregarProducto(@Valid @ModelAttribute("producto") Producto producto, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "nuevoProducto.jsp";
+		} else {
+			sProductos.guardarProducto(producto);
+			return "redirect:/empresa/productos";
+		}
+	}
+
+	// AEeditarProducto.jsp
+	@GetMapping("/editar/producto/{id}")
+	public String editarProducto(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		Producto producto = sProductos.buscarProducto(id);
+
+		if (producto == null) {
+			return "redirect:/empresa/productos";
+		}
+
+		model.addAttribute("producto", producto);
+
+		return "AEeditarProducto.jsp";
+	}
+
+	// PUTmapping
+	@PutMapping("/actualizar/producto/{id}")
+	public String actualizarProducto(@PathVariable("id") Long id, @Valid @ModelAttribute("producto") Producto producto,
+			BindingResult result, Model model, HttpSession session) {
+
+		if (result.hasErrors()) {
+			model.addAttribute("producto", producto);
+			return "AEeditarProducto.jsp";
+		}
+
+		sProductos.guardarProducto(producto);
+
+		return "redirect:/empresa/productos/" + producto.getId();
+	}
+
+	// DELETEmapping
+	@DeleteMapping("/borrar/producto/{id}")
+	public String borrarProducto(@PathVariable("id") Long id) {
+		sProductos.borrarProducto(id);
+		return "redirect:/empresa/productos";
+	}
+
 //ACEpedidos.jsp   *************************************************************** 
-    @GetMapping("/ventas")
-    public String listaVentas(HttpSession session, Model model) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
- 		String cliente = "CLIENTE";//para comparacion en jsp
- 		model.addAttribute("cliente", cliente);
- 		String admin = "ADMIN";
- 		model.addAttribute("admin", admin);
-         
-         //Para conseguir las ventas de LA EMPRESA EN SESION
-         Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
-         model.addAttribute("usuario", usuario);
-         
-        return "ACEpedidos.jsp";
-    }
+	@GetMapping("/ventas")
+	public String listaVentas(HttpSession session, Model model) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		String cliente = "CLIENTE";// para comparacion en jsp
+		model.addAttribute("cliente", cliente);
+		String admin = "ADMIN";
+		model.addAttribute("admin", admin);
+
+		// Para conseguir las ventas de LA EMPRESA EN SESION
+		Usuario usuario = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
+		model.addAttribute("usuario", usuario);
+
+		return "ACEpedidos.jsp";
+	}
+
 //ACEdetallePedido.jsp 
-    @GetMapping("/ventas/{id}")
-    public String detalleVenta(@PathVariable("id") Long id, HttpSession session, Model model) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
-         Pedido pedido = sPedidos.buscarPedido(id);
-         model.addAttribute("pedido", pedido);
-         
- 		Usuario usuarioActualizado = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
- 		model.addAttribute("usuario", usuarioActualizado);
-         
-         return "ACEdetallePedido.jsp";
-    }
+	@GetMapping("/ventas/{id}")
+	public String detalleVenta(@PathVariable("id") Long id, HttpSession session, Model model) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		Pedido pedido = sPedidos.buscarPedido(id);
+		model.addAttribute("pedido", pedido);
+
+		Usuario usuarioActualizado = sUsuarios.buscarUsuario(usuarioEnSesion.getId());
+		model.addAttribute("usuario", usuarioActualizado);
+
+		return "ACEdetallePedido.jsp";
+	}
+
+	// DELETEmapping
+	@DeleteMapping("/borrar/pedido/{id}")
+	public String borrarPedido(@PathVariable("id") Long id) {
+
+		sPedidos.borrarPedido(id);
+
+		return "redirect:/empresa/ventas";
+	}
+
 //AEdetalleEmpresa.jsp    
-    @GetMapping("/{id}")
-    public String detalleEmpresa(@PathVariable("id") Long id, Model model, HttpSession session) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
-        String admin = "ADMIN";
- 		model.addAttribute("admin", admin);
-         
-        Usuario empresa = sUsuarios.buscarUsuario(id);
-        
-        if (empresa == null) {
-            return "redirect:/empresa/home";
-        }
-        
-        model.addAttribute("empresa", empresa);
-        return "AEdetalleEmpresa.jsp";
-    }
-//AEeditarProducto.jsp    
-    @GetMapping("/editar/producto/{id}")
-    public String editarProducto(@PathVariable("id") Long id, Model model, HttpSession session) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
-        Producto producto = sProductos.buscarProducto(id);
-        
-        if (producto == null) {
-            return "redirect:/empresa/productos";
-        }
-        
-        model.addAttribute("producto", producto);
-        
-        return "AEeditarProducto.jsp";
-    }
+	@GetMapping("/{id}")
+	public String detalleEmpresa(@PathVariable("id") Long id, Model model, HttpSession session) {
+
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
+
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		String admin = "ADMIN";
+		model.addAttribute("admin", admin);
+
+		Usuario empresa = sUsuarios.buscarUsuario(id);
+
+		if (empresa == null) {
+			return "redirect:/empresa/home";
+		}
+
+		model.addAttribute("empresa", empresa);
+		return "AEdetalleEmpresa.jsp";
+	}
+
 //AEeditarEmpresa.jsp   
-    @GetMapping("/editar/{id}")
-    public String editar(@ModelAttribute("empresa") Usuario empresa, @PathVariable("id")Long id, Model model, HttpSession session) {
-        
-     	Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
-     	
-         if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
-             return "redirect:/";
-         }
-         
-         Usuario empresaActual = sUsuarios.buscarUsuario(id);
-         
-         if(empresaActual == null || !empresaActual.getId().equals(usuarioEnSesion.getId())) {
-        	 return "redirect:/empresa/"+usuarioEnSesion.getId();
-         }
-         
-         String admin = "ADMIN";//para comparacion en jsp
- 		model.addAttribute("admin", admin);
- 		
-         
-         model.addAttribute("empresa", empresaActual);
-         return "AEeditarEmpresa.jsp";
-    }
-    
-//PUTmapping    
-    @PutMapping("/actualizar/producto/{id}")
-    public String actualizarProducto(@PathVariable("id") Long id, @Valid @ModelAttribute("producto") Producto producto, BindingResult result, Model model, HttpSession session) {
-    	
-        if (result.hasErrors()) {
-            model.addAttribute("producto", producto);
-            return "AEeditarProducto.jsp";
-        }
+	@GetMapping("/editar/{id}")
+	public String editar(@ModelAttribute("empresa") Usuario empresa, @PathVariable("id") Long id, Model model,
+			HttpSession session) {
 
-        sProductos.guardarProducto(producto);
-        
-        return "redirect:/empresa/productos/"+producto.getId();
-    }
-//PUTmapping    
-    @PutMapping("/actualizar/{id}")
-    public String actualizarEmpresa(@Valid @ModelAttribute("empresa") Usuario empresa, BindingResult result, Model model) {
+		Usuario usuarioEnSesion = (Usuario) session.getAttribute("usuarioEnSesion");
 
-        if (result.hasErrors()) {
-            model.addAttribute("empresa", empresa);
-            return "AEeditarEmpresa.jsp";
-        }
-        sUsuarios.guardarUsuario(empresa);
-        return "redirect:/empresa/"+empresa.getId();
-    }
-    
-//DELETEmapping    
-    @DeleteMapping("/borrar/pedido/{id}")
-    public String borrarPedido(@PathVariable("id")Long id) {
-    	
-    	sPedidos.borrarPedido(id);
-    	
-    	return "redirect:/empresa/ventas";
-    }
-//DELETEmapping    
-    @DeleteMapping("/borrar/producto/{id}")
-    public String borrarProducto(@PathVariable("id") Long id) {
-        sProductos.borrarProducto(id);
-        return "redirect:/empresa/productos";
-    }
+		if (session.getAttribute("usuarioEnSesion") == null || !usuarioEnSesion.getTipoDeUsuario().equals("EMPRESA")) {
+			return "redirect:/";
+		}
+
+		Usuario empresaActual = sUsuarios.buscarUsuario(id);
+
+		if (empresaActual == null || !empresaActual.getId().equals(usuarioEnSesion.getId())) {
+			return "redirect:/empresa/" + usuarioEnSesion.getId();
+		}
+
+		String admin = "ADMIN";// para comparacion en jsp
+		model.addAttribute("admin", admin);
+
+		model.addAttribute("empresa", empresaActual);
+		return "AEeditarEmpresa.jsp";
+	}
+
+//PUTmapping    
+	@PutMapping("/actualizar/{id}")
+	public String actualizarEmpresa(@Valid @ModelAttribute("empresa") Usuario empresa, BindingResult result,
+			Model model) {
+
+		if (result.hasErrors()) {
+			model.addAttribute("empresa", empresa);
+			return "AEeditarEmpresa.jsp";
+		}
+		sUsuarios.guardarUsuario(empresa);
+		return "redirect:/empresa/" + empresa.getId();
+	}
+
 }
